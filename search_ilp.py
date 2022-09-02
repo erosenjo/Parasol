206140840466ad334993192f6be67f251ef4d903
mhogan26 <maryh0826@gmail.com>
2022-09-01 20:41:38 -0400

    add ilp vars, hash/mem constraints

--- search_ilp.py
+++ search_ilp.py
@@ -6,9 +6,11 @@ import json
 # pick some (resource-related) variables, use ilp to pick other (resource-related) vars
 # we always want to use as much mem as possible, so obj is max mem (or other limiting resource)
 # TODO:
-#   deps?
-#   how to know if uses hash func? --> assume every reg array uses hash func
+#   deps????
 #   assume 1 reg array per stg?
+#   constraints assume 32 bit regs
+#   CLOSEST POWER OF 2: we can't enforce sol is power of 2 for ints, so we'll round down to closest power of 2 (if we round up, we could use more mem than avail)
+
 # changes from original:
 #   remove phv/metadata constraint
 #   remove tcam constraints
@@ -50,6 +52,8 @@ ilp_vars = opt_info["ilp_vars"]
 const_vars_sizes = {}
 const_vars_ints = {}
 vars_int_size_groups = {} # keep track of which sizes/ints go together
+stages_vars_sizes = [[]]*num_stgs
+stages_vars_ints = [[]]*num_stgs
 # const_vars is list of dicts, each dict contains group of vars (int and size)
 # NOTE: there HAS to be both int and size for each group
 # size is num reg arrays
@@ -71,10 +75,12 @@ for var_group in const_vars:
         # (this makes writing constraints easier, at least for me)
         # name is var,stg_number
         const_vars_sizes[size_name].append(solver.IntVar(0,1, size_name+","+str(n)))
+        stages_vars_sizes[n] = stages_vars_sizes[n] + [const_vars_sizes[size_name][-1]]
 
         # int vars
         # name is var, stg_number
         const_vars_ints[int_name].append(solver.IntVar(0,int_val, int_name+","+str(n)))
+        stages_vars_ints[n] = stages_vars_ints[n] + [const_vars_ints[int_name][-1]]
         # int constr
         # add constraint that reg array width = indicator var * value
         solver.Add(const_vars_ints[int_name][-1] == const_vars_sizes[size_name][-1]*int_val)
@@ -85,20 +91,58 @@ for var_group in const_vars:
     solver.Add(sum(const_vars_sizes[size_name]) == size_val)        
 
 # VARIABLES (not constant)
+ilp_vars_sizes = {}
+ilp_vars_ints = {}
+for var_group in ilp_vars:
+    size_name = var_group["size"]["name"]
+    int_name = var_group["int"]["name"]
+    size_lb = var_group["size"]["lb"]
+    int_lb = var_group["int"]["lb"]
+    vars_int_size_groups[int_name] = size_name
 
+    ilp_vars_sizes[size_name] = []
+    ilp_vars_ints[int_name] = []
 
+    for n in range(num_stgs):
+        # size vars
+        ilp_vars_sizes[size_name].append(solver.IntVar(0,1, size_name+","+str(n)))
+        stages_vars_sizes[n] = stages_vars_sizes[n] + [ilp_vars_sizes[size_name][-1]]
 
+        # int vars
+        ilp_vars_ints[int_name].append(solver.IntVar(0, total_mem, int_name+","+str(n)))
+        stages_vars_ints[n] = stages_vars_ints[n] + [ilp_vars_ints[int_name][-1]]
+        # add constr that arrays not placed have width=0 
+        solver.Add(ilp_vars_ints[int_name][-1] <= ilp_vars_sizes[size_name][-1]*total_mem)
+        # add constr that placed arrays have width > 0
+        solver.Add(ilp_vars_ints[int_name][-1] >= ilp_vars_sizes[size_name][-1])
+
+    # add constr that we at least reach lower bound for size var
+    solver.Add(sum(ilp_vars_sizes[size_name]) >= size_lb)
+
+    # add constr so that regs have the same size
+    # here's how we do this:
+    #   we assume that we have to place AT LEAST one reg array
+    #   width of an array * indicator of first array == width of first array * indicator of array
+    # if we don't place array, it's 0; otherwise it equals width of first array
+    for var in ilp_vars_ints[int_name]:
+        ind = ilp_vars_ints[int_name].index(var)
+        if ind == 0:
+            continue
+        solver.Add(var*ilp_vars_sizes[size_name][0] == ilp_vars_ints[int_name][0]*ilp_vars_sizes[size_name][ind])
 
 # CONSTRAINTS
-# TODO: deps?????????
 # num hashes per stage < hashes
+# NOTE: assume that each reg array uses exactly 1 hash unit
+for stg in stages_vars_sizes:
+    solver.Add(sum(stg) <= hashes)
 
 # amount of mem per stage < total_mem
-
+for stg in stages_vars_ints:
+    solver.Add(sum(stg) <= total_mem)
 
 # simple test objective function
 # TODO: specific to starflow
-solver.Maximize(sum(const_vars_ints["S_SLOTS"]))
+solver.Maximize(sum(ilp_vars_ints["L_SLOTS"]))
 solver.Solve()
 
 print('Solution:')
@@ -111,7 +155,13 @@ for i in const_vars_ints:
     for i_stg in const_vars_ints[i]:
         print(i_stg, '=', i_stg.solution_value())
 
+for s in ilp_vars_sizes:
+    for s_stg in ilp_vars_sizes[s]:
+        print(s_stg, '=', s_stg.solution_value())
 
+for i in ilp_vars_ints:
+    for i_stg in ilp_vars_ints[i]:
+        print(i_stg, '=', i_stg.solution_value())
 
 
 
