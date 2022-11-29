5cdd9db0710263fb4a011041da57275c760377c5
mhogan26 <maryh0826@gmail.com>
2022-11-28 20:09:24 -0500

    fix extern, write to file on last pkt

--- conquest/conquest.py
+++ conquest/conquest.py
@@ -4,9 +4,13 @@ qlens = []
 
 def track_qlen(qlen_est):
     qlens.append(qlen_est)
+    '''
     if len(qlens) > 1021721:
         with open('qlens.pkl','wb') as f:
             pickle.dump(qlens,f)
+    '''
 
-
+def write_to_file():
+    with open('qlens.pkl','wb') as f:
+        pickle.dump(qlens,f)
 
