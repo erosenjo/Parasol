8b17274b658c1135910cdbf7e5e63fe59cfd4bfa
mhogan26 <maryh0826@gmail.com>
2023-01-30 18:42:18 -0500

    clean up externs

--- caching/caching.py
+++ caching/caching.py
@@ -20,50 +20,12 @@ def log_hit(key):
         pickle.dump(hits[0],f)
     '''
 
-# creating sep ones for cms, bc apparently can't have same for both?
-def log_miss_cms(key):
-    misses[0]+=1
-    '''
-    with open('misses.pkl','wb') as f:
-        pickle.dump(misses[0],f)
-    '''
-
-def log_hit_cms(key):
-    hits[0]+=1
-    '''
-    with open('hits.pkl','wb') as f:
-        pickle.dump(hits[0],f)
-    '''
-
-
-# separate externs for hash
-def log_miss_hash(key):
-    misses[0]+=1
-
-def log_hit_hash(key):
-    hits[0]+=1
-
-def write_to_file_cms():
+def write_to_file():
     with open('hits.pkl','wb') as f:
         pickle.dump(hits[0],f)
 
     with open('misses.pkl','wb') as f:
         pickle.dump(misses[0],f)
-
-def write_to_file_precision():
-    with open('misses.pkl','wb') as f:
-        pickle.dump(misses[0],f)
-
-    with open('hits.pkl','wb') as f:
-        pickle.dump(hits[0],f)
-
-def write_to_file_hash():
-    print("HASH WRITE")
-    with open('misses.pkl','wb') as f:
-        pickle.dump(misses[0],f)
-
-    with open('hits.pkl','wb') as f:
-        pickle.dump(hits[0],f)
 
 
 
